360b04318a4ad957a2f65a7b2592e02b0c772977
Adwait Kesharwani <adwait1291@gmail.com>
2022-06-28 15:37:28 +0530

    Update Decision_Tree_Classifier.java

--- Cyber-Bullying-Detection/src/main/java/Decision_Tree_Classifier.java
+++ Cyber-Bullying-Detection/src/main/java/Decision_Tree_Classifier.java
@@ -13,9 +13,9 @@ import org.apache.spark.sql.types.DataTypes;
 public class Decision_Tree_Classifier {
 	public static void main(String[] args) {
 		SparkSession spark = SparkSession
-			    .builder()
-			    .appName("Naive_Bayes")
-			    .master("local[*]")
+			    	.builder()
+			    	.appName("Naive_Bayes")
+			    	.master("local[*]")
 				.getOrCreate();
 			
 				
@@ -23,56 +23,56 @@ public class Decision_Tree_Classifier {
 		String path = "data/data.csv";
 		Dataset<Row> df = spark.read().option("header", "true").csv(path);
 		df.show();
-        df = df.select(
+        	df = df.select(
         		df.col("comments"),
         		df.col("tagging").cast(DataTypes.IntegerType)
-        );		
+       		);		
 	    
-	    Dataset<Row>describe = df.describe();
-	    describe.show();
+	    	Dataset<Row>describe = df.describe();
+	    	describe.show();
 	    
 	    
-	    Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
-	    df = tokenizer.transform(df);
+	    	Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
+	    	df = tokenizer.transform(df);
 		df.show();
 	    
-	    //---------------------------Splitting into train and test set---------------------//
-	    Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
+	    	//---------------------------Splitting into train and test set---------------------//
+	    	Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
 		Dataset<Row> TrainDf = BothTrainTest[0];
 		Dataset<Row> TestDf = BothTrainTest[1];
     
 
-	    CountVectorizerModel cvModel = new CountVectorizer()
-	    		  .setInputCol("words")
-	    	      .setOutputCol("feature")
-	    	      .setVocabSize(20000)
-	    	      .setMinDF(2)
-	    	      .fit(TrainDf);
+	    	CountVectorizerModel cvModel = new CountVectorizer()
+	    		.setInputCol("words")
+	    	      	.setOutputCol("feature")
+	    	      	.setVocabSize(20000)
+	    	      	.setMinDF(2)
+	    	      	.fit(TrainDf);
 	    
-	    TrainDf = cvModel.transform(TrainDf);	  
-	    TestDf = cvModel.transform(TestDf);	    
+	    	TrainDf = cvModel.transform(TrainDf);	  
+	    	TestDf = cvModel.transform(TestDf);	    
 	    
 
-	    VectorAssembler assembler = new VectorAssembler()
+	    	VectorAssembler assembler = new VectorAssembler()
 	    	      .setInputCols(new String[]{"feature"})
 	    	      .setOutputCol("features");
-	    TrainDf = assembler.transform(TrainDf);
-	    TestDf = assembler.transform(TestDf);	
+	    	TrainDf = assembler.transform(TrainDf);
+	    	TestDf = assembler.transform(TestDf);	
 	    
        
 		//---------------------------Model Training---------------------//
 		DecisionTreeClassifier dt = new DecisionTreeClassifier().setLabelCol("tagging");
-        DecisionTreeClassificationModel model = dt.fit(TrainDf);
-	    Dataset<Row> predictions = model.transform(TestDf);
+        	DecisionTreeClassificationModel model = dt.fit(TrainDf);
+	    	Dataset<Row> predictions = model.transform(TestDf);
 
 	   
-	    //---------------------------Printing Accuracy---------------------// 
-	    MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
-	    	    .setLabelCol("tagging")
-	    	    .setPredictionCol("prediction")
-	    	    .setMetricName("accuracy");
-	    	    double accuracy = evaluator.evaluate(predictions);
-	    	    System.out.println("Test set accuracy = " + accuracy);
+	    	//---------------------------Printing Accuracy---------------------// 
+	    	MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+	    		    	.setLabelCol("tagging")
+	    	    		.setPredictionCol("prediction")
+	    	    		.setMetricName("accuracy");
+	    	    		double accuracy = evaluator.evaluate(predictions);
+	    	System.out.println("Test set accuracy = " + accuracy);
 	    
 		spark.stop();
 	}
