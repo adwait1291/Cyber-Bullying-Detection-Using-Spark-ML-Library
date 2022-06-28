b3c67fec41af3e9108b22a0314e246543fe4826b
Adwait Kesharwani <adwait1291@gmail.com>
2022-06-28 15:50:43 +0530

    Update Linear_SVC_TFIDF.java

--- Cyber-Bullying-Detection/src/main/java/Linear_SVC_TFIDF.java
+++ Cyber-Bullying-Detection/src/main/java/Linear_SVC_TFIDF.java
@@ -18,67 +18,69 @@ public class Linear_SVC_TFIDF {
 			      .appName("Naive_Bayes")
 			      .master("local[*]")
 			      .getOrCreate();
+		
 		//---------------------------Loading Dataset---------------------//
 		String path = "data/data.csv";
 		Dataset<Row> df = spark.read().option("header", "true").csv(path);
 		df.show();
-        df = df.select(
+        	df = df.select(
         		df.col("comments"),
         		df.col("tagging").cast(DataTypes.IntegerType)
-        );		
+        	);		
 	    
-	    Dataset<Row>describe = df.describe();
-	    describe.show();
-	    
-	    
-	    Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
-	    df = tokenizer.transform(df);
+	    	Dataset<Row>describe = df.describe();
+	    	describe.show();
 	    
 	    
-	  //---------------------------Splitting into train and test set---------------------//
-	    Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
+	    	Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
+	    	df = tokenizer.transform(df);
+	    
+	    
+	  	//---------------------------Splitting into train and test set---------------------//
+	    	Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
 		Dataset<Row> TrainDf = BothTrainTest[0];
 		Dataset<Row> TestDf = BothTrainTest[1];
-    
+		
+		
+		//---------------------------TF-IDF---------------------//
 		HashingTF hashingTF = new HashingTF()
 			      .setInputCol("words")
 			      .setOutputCol("rawFeatures")
 			      .setNumFeatures(20000);
 				
-			    TrainDf = hashingTF.transform(TrainDf);	  
-			    TestDf = hashingTF.transform(TestDf);	
+		TrainDf = hashingTF.transform(TrainDf);	  
+                TestDf = hashingTF.transform(TestDf);	
 			    
-			    IDF idf = new IDF().setInputCol("rawFeatures").setOutputCol("feature");
-			    IDFModel idfModel = idf.fit(TrainDf);
+		IDF idf = new IDF().setInputCol("rawFeatures").setOutputCol("feature");
+   	        IDFModel idfModel = idf.fit(TrainDf);
 			    
-			    TrainDf = idfModel.transform(TrainDf);	  
-			    TestDf = idfModel.transform(TestDf);
-			    TrainDf.show();
+		TrainDf = idfModel.transform(TrainDf);	  
+		TestDf = idfModel.transform(TestDf);
+		TrainDf.show();
 	    
-	    VectorAssembler assembler = new VectorAssembler()
+	        VectorAssembler assembler = new VectorAssembler()
 	    	      .setInputCols(new String[]{"feature"})
 	    	      .setOutputCol("features");
-	    TrainDf = assembler.transform(TrainDf);
-	    TestDf = assembler.transform(TestDf);	
+	        TrainDf = assembler.transform(TrainDf);
+	    	TestDf = assembler.transform(TestDf);	
 	    
        
-	  //---------------------------Model Training---------------------//
-	    
-	    LinearSVC lsvc = new LinearSVC()
+	 	//---------------------------Model Training---------------------//
+	   	 LinearSVC lsvc = new LinearSVC()
 	    		  .setMaxIter(10)
 	    		  .setRegParam(0.1)
 	    		  .setLabelCol("tagging");
-	    LinearSVCModel model = lsvc.fit(TrainDf);
-	    Dataset<Row> predictions = model.transform(TestDf);
+	    	LinearSVCModel model = lsvc.fit(TrainDf);
+	    	Dataset<Row> predictions = model.transform(TestDf);
 	    
 	    
-	  //---------------------------Printing Accuracy---------------------//
-	    MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+	  	//---------------------------Printing Accuracy---------------------//
+	    	MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
 	    	      .setLabelCol("tagging")
 	    	      .setPredictionCol("prediction")
 	    	      .setMetricName("accuracy");
-	    	    double accuracy = evaluator.evaluate(predictions);
-	    	    System.out.println("Test set accuracy = " + accuracy);
+	    	double accuracy = evaluator.evaluate(predictions);
+	    	System.out.println("Test set accuracy = " + accuracy);
 	    
 	    
 		spark.stop();
