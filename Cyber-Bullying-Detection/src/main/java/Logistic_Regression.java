5d0ebbe09cab8b43fd02f1389b5d024e5b85a5b6
Adwait Kesharwani <adwait1291@gmail.com>
2022-06-28 15:53:15 +0530

    Update Logistic_Regression.java

--- Cyber-Bullying-Detection/src/main/java/Logistic_Regression.java
+++ Cyber-Bullying-Detection/src/main/java/Logistic_Regression.java
@@ -17,64 +17,67 @@ public class Logistic_Regression {
 			      .appName("Naive_Bayes")
 			      .master("local[*]")
 			      .getOrCreate();
+		
 		//---------------------------Loading Dataset---------------------//
 		String path = "data/data.csv";
 		Dataset<Row> df = spark.read().option("header", "true").csv(path);
 		df.show();
-        df = df.select(
+        	df = df.select(
         		df.col("comments"),
         		df.col("tagging").cast(DataTypes.IntegerType)
-        );		
+        	);		
 	    
-	    Dataset<Row>describe = df.describe();
-	    describe.show();
-	    
-	    
-	    Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
-	    df = tokenizer.transform(df);
+	    	Dataset<Row>describe = df.describe();
+	    	describe.show();
 	    
 	    
-	  //---------------------------Splitting into train and test set---------------------//
-	    Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
+	    	Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
+	    	df = tokenizer.transform(df);
+	    
+	    
+	  	//---------------------------Splitting into train and test set---------------------//
+	    	Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
 		Dataset<Row> TrainDf = BothTrainTest[0];
 		Dataset<Row> TestDf = BothTrainTest[1];
-    
+		
+		
+		//---------------------------CountVectorizer---------------------//
 		CountVectorizerModel cvModel = new CountVectorizer()
-	    		  .setInputCol("words")
-	    	      .setOutputCol("feature")
-	    	      .setVocabSize(20000)
-	    	      .setMinDF(2)
-	    	      .fit(TrainDf);
+	    		.setInputCol("words")
+	    	      	.setOutputCol("feature")
+	    	      	.setVocabSize(20000)
+	    	      	.setMinDF(2)
+	    	      	.fit(TrainDf);
 	    
-	    TrainDf = cvModel.transform(TrainDf);	  
-	    TestDf = cvModel.transform(TestDf);	
+	    	TrainDf = cvModel.transform(TrainDf);	  
+	    	TestDf = cvModel.transform(TestDf);	
 	    
 	    
-	    VectorAssembler assembler = new VectorAssembler()
+	    	VectorAssembler assembler = new VectorAssembler()
 	    	      .setInputCols(new String[]{"feature"})
 	    	      .setOutputCol("features");
-	    TrainDf = assembler.transform(TrainDf);
-	    TestDf = assembler.transform(TestDf);	
+	    	TrainDf = assembler.transform(TrainDf);
+	    	TestDf = assembler.transform(TestDf);	
 	    
        
-	  //---------------------------Model Training---------------------//
+	  	//---------------------------Model Training---------------------//
 
-	    LogisticRegression lr = new LogisticRegression()
+	    	LogisticRegression lr = new LogisticRegression()
 	    		  .setMaxIter(10)
 	    		  .setRegParam(0.3)
 	    		  .setElasticNetParam(0.8)
 	    		  .setLabelCol("tagging");
-	    LogisticRegressionModel model = lr.fit(TrainDf);
-	    Dataset<Row> predictions = model.transform(TestDf);
+	    	LogisticRegressionModel model = lr.fit(TrainDf);
+	    	Dataset<Row> predictions = model.transform(TestDf);
 	   
 	    
-	  //---------------------------Printing Accuracy---------------------//
-	    MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+	  	//---------------------------Printing Accuracy---------------------//
+	    	MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
 	    	      .setLabelCol("tagging")
 	    	      .setPredictionCol("prediction")
 	    	      .setMetricName("accuracy");
-	    	    double accuracy = evaluator.evaluate(predictions);
-	    	    System.out.println("Test set accuracy = " + accuracy);
+	    	double accuracy = evaluator.evaluate(predictions);
+	        System.out.println("Test set accuracy = " + accuracy);
 	    
 	    
 		spark.stop();
