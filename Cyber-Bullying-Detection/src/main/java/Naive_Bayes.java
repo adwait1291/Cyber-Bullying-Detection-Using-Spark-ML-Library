0c1c27cf588fc7d1115d3574ab146d3d1521a476
Adwait Kesharwani <adwait1291@gmail.com>
2022-06-28 15:57:41 +0530

    Update Naive_Bayes.java

--- Cyber-Bullying-Detection/src/main/java/Naive_Bayes.java
+++ Cyber-Bullying-Detection/src/main/java/Naive_Bayes.java
@@ -15,59 +15,62 @@ public class Naive_Bayes {
 			      .appName("Naive_Bayes")
 			      .master("local[*]")
 			      .getOrCreate();
+		
 		//---------------------------Loading Dataset---------------------//
 		String path = "data/data.csv";
 		Dataset<Row> df = spark.read().option("header", "true").csv(path);
 		df.show();
-        df = df.select(
+        	df = df.select(
         		df.col("comments"),
         		df.col("tagging").cast(DataTypes.IntegerType)
-        );		
+        	);		
 	    
-	    Dataset<Row>describe = df.describe();
-	    describe.show();
-	    
-	    
-	    Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
-	    df = tokenizer.transform(df);
+	    	Dataset<Row>describe = df.describe();
+	    	describe.show();
 	    
 	    
-	  //---------------------------Splitting into train and test set---------------------//
-	    Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
+	    	Tokenizer tokenizer = new Tokenizer().setInputCol("comments").setOutputCol("words");
+	    	df = tokenizer.transform(df);
+	    
+	    
+	  	//---------------------------Splitting into train and test set---------------------//
+	    	Dataset<Row>[] BothTrainTest = df.randomSplit(new double[] {0.8d,0.2d});
 		Dataset<Row> TrainDf = BothTrainTest[0];
 		Dataset<Row> TestDf = BothTrainTest[1];
     
-	    CountVectorizerModel cvModel = new CountVectorizer()
-	    		  .setInputCol("words")
-	    	      .setOutputCol("feature")
-	    	      .setVocabSize(20000)
-	    	      .setMinDF(2)
-	    	      .fit(TrainDf);
+		
+		//---------------------------CountVectorizer---------------------//
+	    	CountVectorizerModel cvModel = new CountVectorizer()
+	    		.setInputCol("words")
+	    	      	.setOutputCol("feature")
+	    	      	.setVocabSize(20000)
+	    	      	.setMinDF(2)
+	    	      	.fit(TrainDf);
 	    
-	    TrainDf = cvModel.transform(TrainDf);	  
-	    TestDf = cvModel.transform(TestDf);	    
+	    	TrainDf = cvModel.transform(TrainDf);	  
+	    	TestDf = cvModel.transform(TestDf);	    
 	    
 	    
-	    VectorAssembler assembler = new VectorAssembler()
+	    	VectorAssembler assembler = new VectorAssembler()
 	    	      .setInputCols(new String[]{"feature"})
 	    	      .setOutputCol("features");
-	    TrainDf = assembler.transform(TrainDf);
-	    TestDf = assembler.transform(TestDf);	
+	    	TrainDf = assembler.transform(TrainDf);
+	    	TestDf = assembler.transform(TestDf);	
 	    
        
-	  //---------------------------Model Training---------------------//
-	    NaiveBayes nb = new NaiveBayes().setLabelCol("tagging");
-	    NaiveBayesModel model = nb.fit(TrainDf);
-	    Dataset<Row> predictions = model.transform(TestDf);
+	  	//---------------------------Model Training---------------------//
+	    	NaiveBayes nb = new NaiveBayes().setLabelCol("tagging");
+	    	NaiveBayesModel model = nb.fit(TrainDf);
+	    	Dataset<Row> predictions = model.transform(TestDf);
 	    
 	    
-	  //---------------------------Printing Accuracy---------------------//
-	    MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
+	  	//---------------------------Printing Accuracy---------------------//
+	    	MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
 	    	      .setLabelCol("tagging")
 	    	      .setPredictionCol("prediction")
 	    	      .setMetricName("accuracy");
-	    	    double accuracy = evaluator.evaluate(predictions);
-	    	    System.out.println("Test set accuracy = " + accuracy);
+	    	double accuracy = evaluator.evaluate(predictions);
+	    	System.out.println("Test set accuracy = " + accuracy);
 	    
 	    
 		spark.stop();
